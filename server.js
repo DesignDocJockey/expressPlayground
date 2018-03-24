946397e06a9f8247450ea769c2094b500de26d9b
hew0n0 <hwong18@gmail.com>
2018-03-24 13:15:09 -0400

    Adding HttpGet Endpoints

--- server.js
+++ server.js
@@ -12,23 +12,54 @@ server.listen(3000);
 
 const express = require('express');
 const mongoose = require('mongoose');
-
-
-const app = express();
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
+const db = mongoose.connect("mongodb://localhost/courses");
+const Course = require('./models/course');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
 const JAVA_ROUTE = '/java';
+const JAVA_ROUTE_BY_ID = '/java/:courseId';
 
 const javaRouter =  express.Router();
 javaRouter.route(JAVA_ROUTE)
-        .get((request, response) => {
-            let javaCourses = { courses: ['Data Structures in Java for Noobs', 
-                                       'Treehouse - Introduction to Spring',
-                                       'Pluralsight - Java Fundamentals - The Java Language',
-                                       ''
-                                    ]
-                           };
-            response.json(javaCourses);                 
+        .post((request, response) => {
+            let course = new Course();
+
+        })
+        .get((request, response) =>
+        {
+            let query = {};
+            if(request.query.publisher)
+                query.publisher = request.query.publisher.toLowerCase();
+
+            Course.find(query, (error, courses) => {
+                if(error) {
+                    response.status(500).send(error);
+                }
+                else
+                    response.json(courses);
+            });
+        });
+
+javaRouter.route(JAVA_ROUTE_BY_ID)
+        .get( (request, response) => {
+            let courseId = null;
+            if(request.params.courseId && request.params.courseId !== '') {
+                courseId = request.params.courseId;
+            }
+
+            Course.findById(courseId, (error, courses) => {
+                if(error) {
+                    response.status(500).send(error);
+                }
+                else
+                    response.json(courses);
+            });
         });
 
 app.use('/api', javaRouter);
@@ -42,3 +73,11 @@ console.log('Starting Server on Port: ' + PORT);
 app.listen(PORT, ()=> {
     console.log('Listening on Port: ' + PORT);
 });
+
+  /*
+            let javaCourses = { courses: ['Data Structures in Java for Noobs', 
+                                       'Treehouse - Introduction to Spring',
+                                       'Pluralsight - Java Fundamentals - The Java Language',
+                                       ''
+                                    ]
+                           };*/
