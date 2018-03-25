2bce0cf335c613d80c15a2b14313e3e20b51d9a1
hew0n0 <hwong18@gmail.com>
2018-03-24 23:22:00 -0400

    Cleaned up Post endpoints

--- routes/javaroutes.js
+++ routes/javaroutes.js
@@ -1,1 +1,48 @@
 'use strict';
+
+const express = require('express');
+const router =  express.Router();
+
+const BASE_ROUTE = '/';
+const JAVA_ROUTE_BY_ID = '/:courseId';
+
+module.exports = function(Course)
+{
+    router.route(BASE_ROUTE)
+        .post( (request, response) => {
+            let newCourse = new Course(request.body);
+            newCourse.save();
+            response.status(201).send(newCourse);
+        })
+        .get( (request, response) => {
+            let query = {};
+            if(request.query.publisher)
+                query.publisher = request.query.publisher.toLowerCase();
+
+            Course.find(query, (error, courses) => {
+                if(error) {
+                    response.status(500).send(error);
+                }
+                else
+                    response.json(courses);
+            });
+        });
+
+    router.route(JAVA_ROUTE_BY_ID)
+        .get( (request, response) => {
+                let courseId = null;
+                if(request.params.courseId && request.params.courseId !== '') {
+                    courseId = request.params.courseId;
+                }
+
+                Course.findById(courseId, (error, courses) => {
+                    if(error) {
+                        response.status(500).send(error);
+                    }
+                    else
+                        response.json(courses);
+            });
+        });
+
+    return router;
+};
