257a7548e3eac6c72770ac3c8d06a5c53a268489
hew0n0 <hwong18@gmail.com>
2018-03-25 23:23:22 -0400

    Added Patch, Put, & Delete Endpoints; Also Added some Express Middleware

--- routes/javaroutes.js
+++ routes/javaroutes.js
@@ -6,43 +6,101 @@ const router =  express.Router();
 const BASE_ROUTE = '/';
 const JAVA_ROUTE_BY_ID = '/:courseId';
 
-module.exports = function(Course)
+module.exports = (Course) =>
 {
     router.route(BASE_ROUTE)
         .post( (request, response) => {
+            console.log('Post Endpoint Reached ');
+            console.log('Request Body: ' + request.body);
             let newCourse = new Course(request.body);
-            newCourse.save();
-            response.status(201).send(newCourse);
+            newCourse.save( (error) => {
+                if(error)
+                    response.status(500).send(error);
+                else
+                    response.status(201).send(newCourse);
+            });
         })
         .get( (request, response) => {
+            console.log('Get Endpoint Reached ');
+            console.log('Request Query String: ' + request.query);
             let query = {};
             if(request.query.publisher)
                 query.publisher = request.query.publisher.toLowerCase();
 
             Course.find(query, (error, courses) => {
-                if(error) {
+                if(error) 
                     response.status(500).send(error);
-                }
                 else
                     response.json(courses);
             });
         });
 
+    //Implementing Express MiddleWare
+    router.use(JAVA_ROUTE_BY_ID, (request, response, next) => {
+        let courseId = null;
+        if(request.params.courseId && request.params.courseId !== '') {
+            courseId = request.params.courseId;
+        }
+        Course.findById(courseId, (error, course) => {
+            if(error) {
+                response.status(500).send(error);
+            }
+            else if(course) {
+                request.course = course;
+                next();
+            }
+            else
+                response.status(404).send('course not found');
+        });
+    });
+
     router.route(JAVA_ROUTE_BY_ID)
         .get( (request, response) => {
-                let courseId = null;
-                if(request.params.courseId && request.params.courseId !== '') {
-                    courseId = request.params.courseId;
-                }
+               response.json(request.course);
+        })
+        .put( (request, response) => {
+               console.log('Put Endpoint Reached: ' + request.body);
+               console.log('Course in Put Endpoint: ' + request.course);
+               request.course.name = request.body.name;
+               request.course.publisher = request.body.publisher;
+               request.course.subject = request.body.subject;
+               request.course.edition = request.body.edition;
 
-                Course.findById(courseId, (error, courses) => {
-                    if(error) {
+               request.course.save( (error) => {
+                    if(error)
                         response.status(500).send(error);
-                    }
                     else
-                        response.json(courses);
+                        response.json(request.course);
+                });  
+        })
+        .patch( (request, response) => {
+              console.log('Patch Endpoint Reached: ' + request.body);
+              console.log('Course in Patch Endpoint: ' + request.course);
+              if(request.body._id)
+                delete request.body._id;
+
+              for(let property in request.body) {
+                request.course[property] =  request.body[property];
+              }
+
+              request.course.save( (error) => {
+                    if(error)
+                        response.status(500).send(error);
+                    else 
+                        response.json(request.course);
+              });
+        })
+        .delete( (request, response) =>  {
+            console.log('Delete Endpoint Reached: ' + request.body);
+            console.log('Course in Delete Endpoint: ' + request.course);
+            request.course.remove( (error)=> {
+                if(error)
+                    response.status(500).send(error);
+                else
+                    response.status(204).send('Removed');
             });
         });
-
     return router;
 };
+
+//http://erikaybar.name/using-es6-promises-with-mongoosejs-queries/
--- server.js
+++ server.js
@@ -1,15 +1,5 @@
 'use strict';
 
-/* --Basic NodeJs server app
-const http = require('http');
-const app = require('./serverApp');
-
-console.log('Starting Server on port: 3000');
-
-const server = http.createServer(app.serverApp);
-server.listen(3000);
-*/
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -19,8 +9,9 @@ const db = mongoose.connect("mongodb://localhost/courses");
 const Course = require('./models/course');
 const app = express();
 
+app.use(express.static('www'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
 
 const javaRoutes = require('./routes/javaroutes')(Course);
 
@@ -31,12 +22,20 @@ app.get('/', (req, resp) => {
     resp.render('<html><body> Express response for: ' + req.url + ' </body></html>')
 });
 
-console.log('Starting Server on Port: ' + PORT);
 app.listen(PORT, ()=> {
     console.log('Listening on Port: ' + PORT);
 });
 
   /*
+  /* --Basic NodeJs server app
+const http = require('http');
+const app = require('./serverApp');
+
+console.log('Starting Server on port: 3000');
+
+const server = http.createServer(app.serverApp);
+server.listen(3000);
+
             let javaCourses = { courses: ['Data Structures in Java for Noobs', 
                                        'Treehouse - Introduction to Spring',
                                        'Pluralsight - Java Fundamentals - The Java Language',
